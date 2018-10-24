5b1bdeed2403fcc762c2a1a0d7dc7591a705cceb
YoungWenMing <yangwenming99@sina.com>
2018-10-24 20:33:04 +0800

    modify this interface make its sucessor more easy

--- sorting/src/Sort/Sorter.java
+++ sorting/src/Sort/Sorter.java
@@ -7,16 +7,27 @@ public interface Sorter {
     /*
     * return whether v is less than w
     * */
-    boolean less(Comparable v, Comparable w);
+    //boolean less(Comparable v, Comparable w);
 
     /*
     * exchange two items at position a & b
     * */
-    void exch(Comparable[] a, int i, int j);
-
+    static void exch(Comparable[] a, int i, int j){
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
     /*
     * check whether the array is sorted
     * */
-    boolean isSorted(Comparable[] a);
+    static boolean isSorted(Comparable[] a) {
+        for (int i = 1, n = a.length; i < n; i += 1)
+            if (Sorter.less(a[i], a[i - 1])) return false;
+        return true;
+    }
+
+    static boolean less(Comparable v, Comparable w){
+        return v.compareTo(w) < 0;
+    }
 
 }
